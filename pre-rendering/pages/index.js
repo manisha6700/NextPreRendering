e6c81e372f23d29cec16c5c3c81a00fac58117fe
Manisha Jain <manishajain0607@gmail.com>
2022-09-09 22:25:34 +0530

    Pre-rendering in Link

--- pre-rendering/pages/index.js
+++ pre-rendering/pages/index.js
@@ -1,5 +1,19 @@
+import Link from 'next/link'
+
+
 function Home() {
-  return <h1>Next JS pre-rendering</h1>
+  return <>
+  <h1>Next JS pre-rendering</h1>
+  <Link href="/users">
+    <a>Users</a>
+  </Link>
+  {/* while using link the user data in form of
+  json and html is already loaded while loading
+  the index file and hence no additional
+  network fetch is required */}
+  {/* But while routing through the URL individual
+  file and it's components are loaded */}
+  </>
 }
 
 export default Home
